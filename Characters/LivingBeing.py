315fdc13e2786256938cca9ce704e61850210413
C4RN1V0R <lumawa@gmx.ch>
2025-01-23 14:32:44 +0100

    Apply Dice Logick to LivingBeing

--- Characters/LivingBeing.py
+++ Characters/LivingBeing.py
@@ -1,10 +1,11 @@
 from Characters.OwnStat import CharacterStat
+import DiceRoll
 
 class LivingBeing():
-    def __init__(self, hp: int, attackValue: int):
+    def __init__(self, hp: int, attackStr: str):
         self.stats: dict[str: 'CharacterStat'] = {}
         self.stats['HP'] = CharacterStat("hp", hp)
-        self.attackValue = attackValue
+        self.attackDice = DiceRoll.interprateRollString(attackStr)
 
     def attack(self, target: 'LivingBeing'):
-        target.hp.change(self.attackValue)
+        target.hp.change(self.attackDice.roll())
--- Characters/Player.py
+++ Characters/Player.py
@@ -3,7 +3,7 @@ from Characters.OwnStat import CharacterStat
 
 class Player(LivingBeing):
     def __init__(self):
-        super().__init__(100, 7)
+        super().__init__(100, "2d6")
         self.stats['HUNGER'] = CharacterStat("hunger", 0, 100, 10)
         self.stats['THIRST'] = CharacterStat("thirst", 0, 100, 10)
         self.stats['STAMINA'] = CharacterStat("stamina", 100, turnMod= 20)
