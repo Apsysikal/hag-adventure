bef5deecee841c9e466129ef3657072a6b72ae59
C4RN1V0R <lumawa@gmx.ch>
2025-01-23 13:31:27 +0100

    Adding Dice Logic

--- Characters/Player.py
+++ Characters/Player.py
@@ -4,4 +4,6 @@ from Characters.OwnStat import CharacterStat
 class Player(LivingBeing):
     def __init__(self):
         super().__init__(100, 7)
-        self.stats['HUNGER'] = CharacterStat("hunger",100)
+        self.stats['HUNGER'] = CharacterStat("hunger", 0, 100, 10)
+        self.stats['THIRST'] = CharacterStat("thirst", 0, 100, 10)
+        self.stats['STAMINA'] = CharacterStat("stamina", 100, turnMod= 20)
---
+++ DiceRoll.py
@@ -0,0 +1,52 @@
+import random
+import re
+
+regexDice = "^(\d*)d(\d+)$"
+regexRoll = "^(\d*d\d+)((-|\+)(\d*))?"
+
+class Dice():
+    def __init__(self, diceType: int):
+        self.diceType = diceType
+        self.options = range(1, diceType + 1)
+
+    def roll(self) -> int:
+        eyes = random.choice(self.options)
+        print(f"d{self.diceType}: {eyes}")
+        return eyes
+
+
+class DiceRoll():
+    def __init__(self, dices: list, modifier = 0):
+        self.dices = dices
+        self.modifier = modifier
+
+    def roll(self) -> int:
+        totalDices = 0
+        for dice in self.dices:
+            totalDices += dice.roll()
+        total = totalDices + self.modifier
+        print("Total: ", totalDices, " + ", self.modifier, " = ", total)
+
+
+def createDiceList(amount: int, diceType: int) -> list:
+    return [Dice(diceType)] * amount
+
+
+def interprateDiceString(string: str) -> list:
+    match = re.search(regexDice, string)
+    if (match):
+        diceType = int(match.group(2))
+        if (match.group(1)):
+            return createDiceList(int(match.group(1)), diceType)
+        return [Dice(diceType)]
+        
+
+def interprateRollString(string: str) -> DiceRoll:
+    match = re.search(regexRoll, string)
+    if (match):
+        dices = interprateDiceString(match.group(1))
+        if (match.group(2)):
+            if (match.group(3) == "-"):
+                return DiceRoll(dices, 0 - int(match.group(4)))
+            return DiceRoll(dices, int(match.group(4)))
+        return DiceRoll(dices)
